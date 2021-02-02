9c248cef57440288400630fa8763804ea6505a65
lntel <joeharris461767@gmail.com>
2021-02-02 23:44:22 +0000

    Added email regex

--- backend/src/validators/user.ts
+++ backend/src/validators/user.ts
@@ -2,5 +2,6 @@ import { ValidatorOptions } from "../types";
 
 export const createUserValidator: ValidatorOptions = {
     key: 'emailAddress',
-    name: 'Email address'
+    name: 'Email address',
+    expression: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/g
 }
