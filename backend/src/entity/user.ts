c81cc04e935bdf8df7fcd4b900f3b34e9ca9c741
lntel <joeharris461767@gmail.com>
2021-02-02 18:38:49 +0000

    Added user entity

---
+++ backend/src/entity/user.ts
@@ -0,0 +1,12 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User {
+
+    @PrimaryGeneratedColumn("uuid")
+    id: string;
+
+    @Column()
+    username: string;
+
+}
