935e4d07a172b2ee7c5e3593510bda9df5bf3fc6
lntel <joeharris461767@gmail.com>
2021-02-02 20:38:26 +0000

    Implemented creation validation

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { createUser, login } from "../controllers/user";
 import { validate } from "../middleware/validate";
+import { createUserValidator } from '../validators/user'
 
 const router = Router();
 
 // Create a new user
-router.post('/', createUser);
+router.post('/', validate(createUserValidator), createUser);
 
 // Login to a user account
 router.post('/login', login);
