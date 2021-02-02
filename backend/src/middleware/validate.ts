bbed2f8169ab5f0b2a5a7d3b0fe19e3dd271abb1
lntel <joeharris461767@gmail.com>
2021-02-02 20:38:15 +0000

    Added key check validation

--- backend/src/middleware/validate.ts
+++ backend/src/middleware/validate.ts
@@ -1,10 +1,27 @@
 import { NextFunction, Request, Response } from "express"
 import { ValidatorOptions } from "../types"
 
+
 export const validate = (e: ValidatorOptions | ValidatorOptions[]) => {
     return (req: Request, res: Response, next: NextFunction) => {
 
-        console.log(e)
+        const bodyKeys = Object.keys(req.body);
+
+        if((e as ValidatorOptions[]).length) {
+
+        } else {
+            if(bodyKeys.length) {
+                if(bodyKeys.indexOf((e as ValidatorOptions).key) == -1) {
+                    return res.status(400).send({
+                        message: `${(e as ValidatorOptions).name ? (e as ValidatorOptions).name : (e as ValidatorOptions).key} parameter was not found`
+                    });
+                }
+            } else {
+                return res.status(400).send({
+                    message: 'No parameters found'
+                });
+            }
+        }
 
     }
 }
