894c3d17917ab53ec8a9437199c605d5d5af5985
lntel <joeharris461767@gmail.com>
2021-02-02 18:37:42 +0000

    Added typeorm connection Added try catch Added user router

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,39 @@
+import 'reflect-metadata'
 import express from 'express';
+import { createConnection } from 'typeorm';
 import config from './config';
+import { User } from './entity/user';
 
-const app = express();
+import userRouter from './routes/user'
 
-app.listen(config.apiPort, () => {
-    console.log(`http://${config.host}:${config.apiPort} -> Online`);
-});
+(async () => {
+    const app = express();
+
+    try {
+        const connection = await createConnection({
+            type: 'postgres',
+            host: config.host,
+            port: 5432,
+            username: config.dbUser,
+            password: config.dbPass,
+            database: config.dbName,
+            synchronize: true,
+            entities: [
+                User
+            ]
+        });
+
+        console.log(`Posgresql -> Online`);
+
+        app.use('/api/v1/user', userRouter);
+        
+        app.listen(config.apiPort, () => {
+            console.log(`http://${config.host}:${config.apiPort} -> Online`);
+        });
+
+    }
+    catch(err) {
+        console.error(err);
+    }
+
+})();
