e192c386a6053df1ba578c74706ee25a29355b28
lntel <joeharris461767@gmail.com>
2021-02-08 20:56:53 +0000

    Added deleteById

--- src/routes/site.ts
+++ src/routes/site.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { createSite, getAllSites } from "../controllers/site";
+import { createSite, deleteById, getAllSites } from "../controllers/site";
 import isAdmin from "../middleware/isAdmin";
 import isLoggedIn from "../middleware/isLoggedIn";
 
 const router = Router();
 
 router.get('/', isLoggedIn, getAllSites);
+
+router.delete('/:siteId', isAdmin, deleteById);
 
 router.post('/', isAdmin, createSite);
 
