2251e8ba44892b57d1cb1f713cd098bd1031bfed
lntel <joeharris461767@gmail.com>
2021-02-08 13:04:52 +0000

    Added account creation parameters

--- src/validators/user.ts
+++ src/validators/user.ts
@@ -1,10 +1,16 @@
 import { ValidatorOptions } from "../types";
 
-export const createUserValidator: ValidatorOptions = {
+export const createUserValidator: ValidatorOptions[] = [{
     key: 'emailAddress',
     name: 'Email address',
     expression: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/g
-}
+}, {
+    key: 'password',
+    name: 'Password',
+}, {
+    key: 'mobileNumber',
+    name: 'Mobile Number'
+}]
 
 export const loginUserValidator: ValidatorOptions[] = [
     {
