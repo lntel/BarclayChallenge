3efb8b4cedc4656f6d7002266b23eaa85e4fdf92
cosmin2iordache <cosmin2iordache@gmail.com>
2021-02-08 13:18:00 +0000

    hello

--- src/entity/payment.ts
+++ src/entity/payment.ts
@@ -10,16 +10,39 @@ export class Payment {
     emailAddress: string;
 
     @Column()
-    password: string;
+    locale: string;
+    
+    @Column()
+    transaction_type: string;
+    
+    @Column()
+    reference_number: number;
+    
+    @Column()
+    amount: number;
 
-    public static async exists(emailAddress: string): Promise<Payment | undefined> {
-        const userRepo = getRepository(Payment);
+    @Column()
+    $currency = $_POST['currency'];
+    @Column()
+    $signed_date_time = $_POST['signed_date_time'];    
+    $access_key = $_POST['access_key'];
+    $profile_id = $_POST['profile_id'];
+    $signed_field_names = $_POST['signed_field_names'];
+    $unsigned_field_names = $_POST['unsigned_field_names'];
+    $bill_to_address_city = $_POST['bill_to_address_city'];
+    $bill_to_address_country = $_POST['bill_to_address_country'];
+    $bill_to_address_line1 = $_POST['bill_to_address_line1'];
+    $bill_to_address_postal_code = $_POST['bill_to_address_postal_code'];
+    $bill_to_email = $_POST['bill_to_email'];
+    $bill_to_forename = $_POST['bill_to_forename'];
+    $bill_to_surname = $_POST['bill_to_surname'];
 
-        return await userRepo.findOne({
-            where: {
-                emailAddress: emailAddress.toLowerCase()
-            }
-        });
-    }
+
+
+
+
+
+
+
 
 }
