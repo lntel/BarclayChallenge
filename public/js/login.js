ca1d651423e69bfd7b9fc33ec09c02c805e31f6c
lntel <joeharris461767@gmail.com>
2021-02-08 16:58:15 +0000

    Added login functionality

---
+++ public/js/login.js
@@ -0,0 +1,25 @@
+const loginBtn = document.querySelector('#login-btn');
+
+const successMsg = document.querySelector('#msgbox');
+
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+
+loginBtn.addEventListener('click', async (e) => {
+    e.preventDefault();
+
+    try {
+        const response = await Request('user/login', {
+            type: 'POST',
+            data: {
+                emailAddress: email.value,
+                password: password.value
+            }
+        });
+
+
+    }
+    catch(err) {
+        $('#msgbox').toast(option)
+    }
+})
