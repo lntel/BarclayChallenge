1d38a47b437936ca58f8f8964465bdd8c802f6c7
lntel <joeharris461767@gmail.com>
2021-02-08 16:05:27 +0000

    Added isAdmin middleware

--- src/middleware/isAdmin.ts
+++ src/middleware/isAdmin.ts
@@ -1,3 +1,18 @@
-export default () => {
-    
+import { Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+import config from "../config";
+
+export default (req: Request, res: Response) => {
+
+    const { accessToken } = req.cookies;
+
+    try {
+        const result = verify(accessToken, config.accessSecret);
+
+        
+    }
+    catch(err) {
+        console.error(err);
+    }
+
 }
