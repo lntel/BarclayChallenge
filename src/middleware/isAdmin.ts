f4a070622b40beb9d6dd5ef3d8335e885be7d0aa
lntel <joeharris461767@gmail.com>
2021-02-08 17:01:04 +0000

    Added isAdmin middleware

--- src/middleware/isAdmin.ts
+++ src/middleware/isAdmin.ts
@@ -1,15 +1,26 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { verify } from "jsonwebtoken";
 import config from "../config";
+import { User } from "../entity/user";
 
-export default (req: Request, res: Response) => {
+export default async (req: Request, res: Response, next: NextFunction) => {
 
     const { accessToken } = req.cookies;
 
     try {
-        const result = verify(accessToken, config.accessSecret);
 
-        
+        console.log("test")
+        const result = verify(accessToken, config.accessSecret) as any;
+
+        if(!await User.isAdmin(result.id)) {
+            return res.status(403).send({
+                message: 'Only admins can access this route'
+            });
+        }
+
+        req.params.id = result.id;
+
+        next();
     }
     catch(err) {
         console.error(err);
