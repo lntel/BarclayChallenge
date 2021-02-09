6dec80c9d5890799876a27ef29ded358dc939109
lntel <joeharris461767@gmail.com>
2021-02-09 08:38:08 +0000

    Added product get request

---
+++ public/js/product.js
@@ -0,0 +1,36 @@
+const row = document.querySelector('.row');
+
+const getProducts = async () => {
+
+    try {
+        const products = await Request('product', {
+            type: 'GET'
+        });
+
+        products.data.map(product => {
+            const e = document.createElement('div');
+
+            e.className = 'col-lg-4 products col-xl-3 col-md-12';
+
+            e.innerHTML = `
+            <div class="card">
+                <img class="card-img-top" src="${product.imageUrl}" alt="Card image cap">
+                <div class="card-body">
+                <h5 class="card-title">${product.name}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">£${product.price}</h6>
+                <p class="card-text">${product.description}</p>
+                <a href="#" class="btn btn-primary">Add to basket</a>
+                </div>
+            </div>
+            `;
+
+            row.appendChild(e);
+        });
+
+    } catch (error) {
+        console.error(error);
+    }
+    
+}
+
+getProducts();
