d90bce6aa74440293ce681d11a66ad9d320ee0dc
lntel <joeharris461767@gmail.com>
2021-02-09 12:16:21 +0000

    Added delete route

--- src/routes/product.ts
+++ src/routes/product.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { createProduct, getAllProducts } from "../controllers/product";
+import { createProduct, deleteProduct, getAllProducts } from "../controllers/product";
 import isAdmin from "../middleware/isAdmin";
 
 const router = Router();
 
 router.get('/', getAllProducts);
+
+router.delete('/:productId', isAdmin, deleteProduct);
 
 router.post('/', isAdmin, createProduct)
 
