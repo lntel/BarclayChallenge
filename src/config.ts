e229b7e7ffd20e3e2c66cb4e909dc133f6a2361e
lntel <joeharris461767@gmail.com>
2021-03-03 23:34:50 +0000

    Added port env

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 const config: Config = {
     host: process.env.HOST || 'localhost',
-    apiPort: (process.env.API_PORT as unknown as number) || 3000,
+    apiPort: (process.env.PORT as unknown as number) || 3000,
     dbPort: (process.env.DB_PORT as unknown as number) || 5432,
     dbUser: process.env.DB_USER || 'postgres',
     dbPass: process.env.DB_PASS || 'test',
